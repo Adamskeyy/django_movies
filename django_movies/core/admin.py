f13d30732a1c0363beff3e49b143e62791733556
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-09-06 11:47:02 +0200

    Modified model

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Movie, Genre
+from django.urls import path
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+AGE_CHOICES = (
+    (7, '7'),
+    (13, '13'),
+    (16, '16'),
+    (18, '18'),
+    (21, '21')
+)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age_limit = models.IntegerField(null=True, blank=True, choices=AGE_CHOICES)
+
+    def __str__(self):
+        return self.name
+    
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(
-        null=True, validators=[MaxValueValidator(10), MinValueValidator(1)]
+        null=True, validators=[MaxValueValidator(15), MinValueValidator(1)]
     )
     released = models.DateField()
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
